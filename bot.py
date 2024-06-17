201506518648fc33fba73627a0452cf8dad72fc0
Karanka <154072716+gitcitt1@users.noreply.github.com>
2024-06-17 20:59:54 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,12 +5,13 @@ from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
 default_format = "{time} | {level} | {message}"
 
 nonebot.init()
-logger.add("trace.log", level="TRACE", format=default_format)
+#  logger.add("trace.log", level="TRACE", format=default_format)
+#  输出日志到文件谨慎开启，image2text的日志非超多，不一会就能写出几百MB
 
 driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11Adapter)
 
-nonebot.load_plugins("src/plugins")
+nonebot.load_plugins("mybot/plugins")
 
 if __name__ == "__main__":
     nonebot.run()
